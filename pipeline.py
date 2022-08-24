26ec4b66d2573818f1df333c1e4516f2166d84d1
bryantkrause <bryantkrause@gmail.com>
2022-08-24 14:52:45 -0700

    row data does not equal tuple

--- pipeline.py
+++ pipeline.py
@@ -5,7 +5,6 @@ import os
 import queries
 
 data = []
-
 # get connection data
 load_dotenv('.env')
 DSN =os.environ.get("DSN")
@@ -14,22 +13,17 @@ PWD =os.environ.get("PWD")
 
 # establish connection
 conn = pyodbc.connect(f'DSN={DSN};UID={UID};PWD={PWD}')
-
-# test connection
+# set connection
 cursor = conn.cursor()
-
+# run query
 cursor.execute(queries.palletsRec())
-
-for i in cursor:
-    data.append(i)
-
-print(data)
-
-df = pd.DataFrame(data)
-print(df)
-
+# convert raw data to list format
+for  row in cursor:
+    row_to_list = [elem for elem in row]
+    data.append(row_to_list)  
+# add data to data frame
+df = pd.DataFrame(data, columns=['Number','Year','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
+# write excel file
 writer = pd.ExcelWriter('PalletsRecieved.xlsx', engine='xlsxwriter')
-
 df.to_excel(writer, sheet_name='PalletsRecieved')
-
 writer.save()
--- queries.py
+++ queries.py
@@ -5,7 +5,6 @@ def palletsRec():
     year(DeliveryDate) as year, IsNull(month(DeliveryDate), 0) as month,
     PalletsReceived FROM WarehouseReceipt WHERE DeliveryDate BETWEEN '1/01/2021 00:00:01' AND '07/31/2022 23:59:59' AND 
     CustomerName != 'PC' AND CustomerName != 'Z_TEST')  test
-    PIVOT ( SUM(PalletsReceived) FOR month IN ([1] , [2] , [3] , [4] , [5] , [6] , [7] , [8] ,[9] , [10] , [11] , [12])) AS thingy ORDER BY year desc"""    
+    PIVOT ( SUM(PalletsReceived) FOR month IN ([1] , [2] , [3] , [4] , [5] , [6] , [7] , [8] ,[9] , [10] , [11] , [12])) AS thingy ORDER BY ID, year desc"""
     return palletsRecQuery
 
-# 'SELECT CASE (WHEN CustomerName >= 430 THEN 430 ELSE CustomerName END) As ID,'\
