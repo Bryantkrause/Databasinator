e220c89b14bcbb4ed988108dbc589a57817c0522
bryantkrause <bryantkrause@gmail.com>
2022-08-24 16:31:18 -0700

    I HAVE MADE FIRE

--- pipeline.py
+++ pipeline.py
@@ -16,14 +16,15 @@ conn = pyodbc.connect(f'DSN={DSN};UID={UID};PWD={PWD}')
 # set connection
 cursor = conn.cursor()
 # run query
-cursor.execute(queries.palletsRec())
+cursor.execute(queries.storageByUnit())
 # convert raw data to list format
 for  row in cursor:
     row_to_list = [elem for elem in row]
     data.append(row_to_list)  
 # add data to data frame
-df = pd.DataFrame(data, columns=['Number','Year','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
+# df = pd.DataFrame(data, columns=['Number','Year','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
+df = pd.DataFrame(data)
 # write excel file
-writer = pd.ExcelWriter('PalletsRecieved.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('Storage.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='PalletsRecieved')
 writer.save()
--- queries.py
+++ queries.py
@@ -8,3 +8,15 @@ def palletsRec():
     PIVOT ( SUM(PalletsReceived) FOR month IN ([1] , [2] , [3] , [4] , [5] , [6] , [7] , [8] ,[9] , [10] , [11] , [12])) AS thingy ORDER BY ID, year desc"""
     return palletsRecQuery
 
+def storageByUnit():
+    storage = """
+SELECT 
+CASE WHEN CustomerName >= 430 THEN 430 ELSE CustomerName END As ID,
+FORMAT(EffectiveDate, 'yyyyMM') AS YM,
+TransactionType, Sum(QTY) As QTY
+FROM InventoryTransaction
+WHERE CustomerName = '425' AND FacilityName = 'Fullerton' AND TransactionType <4 
+GROUP BY CustomerName ,  TransactionType, FORMAT(EffectiveDate, 'yyyyMM')
+ORDER BY FORMAT(EffectiveDate, 'yyyyMM')
+    """
+    return storage
